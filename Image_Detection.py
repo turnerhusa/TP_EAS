c8e7a89b7c5273b31167ca4fa2c2024958d9b27b
Turner Halligan <turnerhusa@gmail.com>
2020-02-15 14:51:18 -0500

    Clean up image detection

--- Image_Detection.py
+++ Image_Detection.py
@@ -20,7 +20,6 @@ high_threshold = float(sys.argv[4])
 
 #load and display original image as grayscale
 image = skimage.io.imread(fname=filename, as_gray=True)
-#skimage.io.imread(fname="citySkyLine.jpeg", as_gray=True)
 
 
 edge_roberts = roberts(image)
@@ -33,8 +32,6 @@ edges = skimage.feature.canny(
     high_threshold=high_threshold,
 )
 
-#shapes = EdgeProcessor.processEdges(edges)
-#print(shapes)
 
 contours = measure.find_contours(edges, 0.8)
 
@@ -44,8 +41,6 @@ numOfContours = len(contours)
 for n in range(0,numOfContours):
 	if (n > 0):
 		#insert connection line
-		#print(contours[n-1][-1][0],contours[n-1][-1][1])
-		#print(n)
 		newContour_stack.put(( [contours[n-1][-1][0], contours[n-1][-1][1]], [contours[n][0][0], contours[n][0][1]] ))
 	else:
 		pass
@@ -61,22 +56,8 @@ while (not newContour_stack.empty()):
 	reverseIndex -= 1;
 
 for n, contour in enumerate(contours):
-	#print(contour)
-	#print(contour);
 	plt.plot(contour[:, 1], contour[:, 0], linewidth=2)
 
-# for a in ax:
-#     a.axis('off')
-
-# plt.tight_layout()
 plt.show()
 
-#display edges
-#viewer = skimage.viewer.ImageViewer(edges)
-#viewer.show()
 
-# TODO use this to save the file for processing 
-# save binary image; first find beginning of file extension
-# dot = filename.index(".")
-# binary_file_name = filename[:dot] + "-binary" + filename[dot:]
-# skimage.io.imsave(fname=binary_file_name, arr=skimage.img_as_ubyte(binary))
