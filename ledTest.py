da1be87c3109c47310e4fe6d4655db1242278542
Steven Montalbano <stevenmontalbano@gmail.com>
2020-02-14 22:16:22 -0500

    control test files

---
+++ ledTest.py
@@ -0,0 +1,26 @@
+import serial
+import time
+
+aData = serial.Serial('/dev/tty.usbmodem14201', baudrate=9600)
+
+#aData = serial.Serial('/dev/tty.usbmodem14101', baudrate=9600)
+
+
+def ledOn():
+    aData.write(str.encode('1'))
+
+def ledOff():
+    aData.write(str.encode('0'))
+
+
+i = 0
+
+while(i < 100):
+    print(i)
+
+    if(i % 2 == 0):
+        ledOn()
+    else:
+        ledOff()
+    time.sleep(1)
+    i += 1
